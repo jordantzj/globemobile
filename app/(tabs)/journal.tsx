8808015e4a3b2cfdc3bfb0bfda8970fb6e834acf
Jordan Tay <jordantay226@gmail.com>
2025-01-14 22:05:16 +0800

    added countries-by-continent.json

--- app/(tabs)/journal.tsx
+++ app/(tabs)/journal.tsx
@@ -4,6 +4,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
 import CountryContainer from '@/components/CountryContainer';
+import countries from '@/app/country-by-continent.json'
 
 export default function TabTwoScreen() {
   return (
@@ -19,28 +20,75 @@ export default function TabTwoScreen() {
       </ThemedView>
       <ThemedView>
         <ThemedText type="defaultSemiBold">Europe</ThemedText>
-        <CountryContainer countryName="France" continent="EU" />
-        <CountryContainer countryName="Germany" continent="EU" />
+        {countries
+        .filter((country) => country.continent === "Europe")
+        .map((country) => (
+          <CountryContainer
+            key={country.country}
+            countryName={country.country}
+            continent="EU"
+          />
+        ))}
       </ThemedView>
       <ThemedView>
         <ThemedText type="defaultSemiBold">Asia</ThemedText>
-        <CountryContainer countryName="China" continent="AS" />
+        {countries
+        .filter((country) => country.continent === "Asia")
+        .map((country) => (
+          <CountryContainer
+            key={country.country}
+            countryName={country.country}
+            continent="AS"
+          />
+        ))}
       </ThemedView>
       <ThemedView>
         <ThemedText type="defaultSemiBold">Africa</ThemedText>
-        <CountryContainer countryName="Egypt" continent="AF" />
+        {countries
+        .filter((country) => country.continent === "Africa")
+        .map((country) => (
+          <CountryContainer
+            key={country.country}
+            countryName={country.country}
+            continent="AF"
+          />
+        ))}
       </ThemedView>
       <ThemedView>
         <ThemedText type="defaultSemiBold">North America</ThemedText>
-        <CountryContainer countryName="Canda" continent="NA" />
+        {countries
+        .filter((country) => country.continent === "North America")
+        .map((country) => (
+          <CountryContainer
+            key={country.country}
+            countryName={country.country}
+            continent="NA"
+          />
+        ))}
       </ThemedView>
       <ThemedView>
         <ThemedText type="defaultSemiBold">South America</ThemedText>
-        <CountryContainer countryName="Brazil" continent="SA" />
+        {countries
+        .filter((country) => country.continent === "South America")
+        .map((country) => (
+          <CountryContainer
+            key={country.country}
+            countryName={country.country}
+            continent="SA"
+          />
+        ))}
       </ThemedView>
       <ThemedView>
         <ThemedText type="defaultSemiBold">Oceania</ThemedText>
-        <CountryContainer countryName="Australia" continent="OC" />
+        {countries
+        .filter((country) => country.continent === "Oceania")
+        .map((country) => (
+          <CountryContainer
+            key={country.country}
+            countryName={country.country}
+            continent="OC"
+          />
+        ))}
       </ThemedView>
     </ParallaxScrollView>
   );
